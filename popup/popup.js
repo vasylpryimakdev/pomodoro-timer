ef941b0cebcffb130794f07cfb488a8ff6d02b71
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-07 19:12:32 +0300

    feat: Add reset timer functionality

--- popup/popup.js
+++ popup/popup.js
@@ -3,12 +3,30 @@ let tasks = [];
 const startTimerBtn = document.getElementById("start-timer-btn");
 startTimerBtn.addEventListener("click", () => {
   chrome.storage.local.get(["isRunning"], (res) => {
-    chrome.storage.local.set({
-      isRunning: !res.isRunning,
-    }, () => {
-      startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
-    });
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? "Pause Timer"
+          : "Start Timer";
+      },
+    );
   });
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+    },
+  );
 });
 
 const addTaskBtn = document.getElementById("add-task-btn");
