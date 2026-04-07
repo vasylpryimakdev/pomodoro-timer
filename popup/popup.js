e818b8a552c4f11d99b6f78619a3abf8af38274c
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-07 17:20:42 +0300

    feat: implement task data management and event handling

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,5 @@
+const tasks = [];
+
 const addTaskBtn = document.getElementById("add-task-btn");
 
 addTaskBtn.addEventListener("click", () => {
@@ -5,14 +7,23 @@ addTaskBtn.addEventListener("click", () => {
 });
 
 function addTask() {
+  const taskNum = tasks.length;
+  tasks.push("");
+
   const taskRow = document.createElement("div");
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task";
+  text.addEventListener("change", () => {
+    tasks[taskNum] = text.value;
+  });
 
   const deleteBtn = document.createElement("input");
   deleteBtn.type = "button";
   deleteBtn.value = "X";
+  deleteBtn.addEventListener("click", () => {
+    tasks.splice(taskNum, 1);
+  });
 
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
