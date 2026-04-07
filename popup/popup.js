ebc195fbf669f5ee831fba129a46a0d3bfe4c7fa
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-07 17:43:54 +0300

    feat: add persistent task storage with Chrome sync

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,20 @@
-const tasks = [];
+let tasks = [];
 
 const addTaskBtn = document.getElementById("add-task-btn");
-
 addTaskBtn.addEventListener("click", () => {
   addTask();
 });
+
+chrome.storage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
+
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasks,
+  });
+}
 
 function renderSingleTask(taskNum) {
   const taskRow = document.createElement("div");
@@ -14,6 +24,7 @@ function renderSingleTask(taskNum) {
   text.value = tasks[taskNum];
   text.addEventListener("change", () => {
     tasks[taskNum] = text.value;
+    saveTasks();
   });
 
   const deleteBtn = document.createElement("input");
@@ -34,11 +45,13 @@ function addTask() {
   const taskNum = tasks.length;
   tasks.push("");
   renderSingleTask(taskNum);
+  saveTasks();
 }
 
 function deleteTask(taskNum) {
   tasks.splice(taskNum, 1);
   renderTasks();
+  saveTasks();
 }
 
 function renderTasks() {
