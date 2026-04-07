0ddd7299cc59c848bf6980236c9b00dae22c873d
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-07 17:13:28 +0300

    feat: add task management functionality

---
+++ popup/popup.js
@@ -0,0 +1,22 @@
+const addTaskBtn = document.getElementById("add-task-btn");
+
+addTaskBtn.addEventListener("click", () => {
+  addTask();
+});
+
+function addTask() {
+  const taskRow = document.createElement("div");
+  const text = document.createElement("input");
+  text.type = "text";
+  text.placeholder = "Enter a task";
+
+  const deleteBtn = document.createElement("input");
+  deleteBtn.type = "button";
+  deleteBtn.value = "X";
+
+  taskRow.appendChild(text);
+  taskRow.appendChild(deleteBtn);
+
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.appendChild(taskRow);
+}
