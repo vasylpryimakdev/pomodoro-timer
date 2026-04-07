ee6ffa1717164e2704446eb8dbb4a3d1eb837763
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-07 21:19:52 +0300

    refactor: Format code and update notification system

--- options/options.js
+++ options/options.js
@@ -1,22 +1,20 @@
-const timeOption = document.getElementById("time-option");
+const timeOption = document.getElementById("time-option")
 timeOption.addEventListener("change", (event) => {
-  const val = event.target.value;
+    const val = event.target.value
+    if (val < 1 || val > 60) {
+        timeOption.value = 25
+    }
+})
 
-  if (val < 1 || val > 60) {
-    timeOption.value = 25;
-  }
-});
-
-const saveBtn = document.getElementById("save-btn");
-
+const saveBtn = document.getElementById("save-btn")
 saveBtn.addEventListener("click", () => {
-  chrome.storage.local.set({
-    timer: 0,
-    isRunning: false,
-    timeOption: timeOption.value,
-  });
-});
+    chrome.storage.local.set({
+        timer: 0,
+        timeOption: timeOption.value,
+        isRunning: false,
+    })
+})
 
-chrome.storage.local.get("timeOption", (res) => {
-  timeOption.value = res.timeOption;
-});
+chrome.storage.local.get(["timeOption"], (res) => {
+    timeOption.value = res.timeOption
+})
--- popup/popup.js
+++ popup/popup.js
@@ -1,113 +1,101 @@
-let tasks = [];
+let tasks = []
 
 function updateTime() {
-  chrome.storage.local.get(["timer", "timeOption"], (res) => {
-    const time = document.getElementById("time");
-    const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(
-      2,
-      "0",
-    );
-
-    let seconds = "00";
-
-    if (res.timer % 60 !== 0) {
-      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
-    }
-
-    time.textContent = `${minutes}:${seconds}`;
-  });
+    chrome.storage.local.get(["timer", "timeOption", "isRunning"], (res) => {
+        const time = document.getElementById("time")
+        const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, "0")
+        let seconds = "00"
+        if (res.timer % 60 != 0) {
+            seconds = `${60 - res.timer % 60}`.padStart(2, "0")
+        }
+        time.textContent = `${minutes}:${seconds}`
+        startTimerBtn.textContent = res.isRunning ? "Pause Timer" : "Start Timer"
+    })
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+updateTime()
+setInterval(updateTime, 1000)
 
-const startTimerBtn = document.getElementById("start-timer-btn");
+const startTimerBtn = document.getElementById("start-timer-btn")
 startTimerBtn.addEventListener("click", () => {
-  chrome.storage.local.get(["isRunning"], (res) => {
-    chrome.storage.local.set(
-      {
-        isRunning: !res.isRunning,
-      },
-      () => {
-        startTimerBtn.textContent = !res.isRunning
-          ? "Pause Timer"
-          : "Start Timer";
-      },
-    );
-  });
-});
+    chrome.storage.local.get(["isRunning"], (res) => {
+        chrome.storage.local.set({
+            isRunning: !res.isRunning,
+        }, () => {
+            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
+        })
+    })
+})
 
-const resetTimerBtn = document.getElementById("reset-timer-btn");
+const resetTimerBtn = document.getElementById("reset-timer-btn")
 resetTimerBtn.addEventListener("click", () => {
-  chrome.storage.local.set(
-    {
-      timer: 0,
-      isRunning: false,
-    },
-    () => {
-      startTimerBtn.textContent = "Start Timer";
-    },
-  );
-});
+    chrome.storage.local.set({
+        timer: 0,
+        isRunning: false,
+    }, () => {
+        startTimerBtn.textContent = "Start Timer"
+    })
+})
 
-const addTaskBtn = document.getElementById("add-task-btn");
-addTaskBtn.addEventListener("click", () => {
-  addTask();
-});
+const addTaskBtn = document.getElementById("add-task-btn")
+addTaskBtn.addEventListener("click", () => addTask())
 
 chrome.storage.sync.get(["tasks"], (res) => {
-  tasks = res.tasks ? res.tasks : [];
-  renderTasks();
-});
+    tasks = res.tasks ? res.tasks : []
+    renderTasks()
+})
 
 function saveTasks() {
-  chrome.storage.sync.set({
-    tasks,
-  });
+    chrome.storage.sync.set({
+        tasks,
+    })
 }
 
-function renderSingleTask(taskNum) {
-  const taskRow = document.createElement("div");
-  const text = document.createElement("input");
-  text.type = "text";
-  text.placeholder = "Enter a task";
-  text.value = tasks[taskNum];
-  text.addEventListener("change", () => {
-    tasks[taskNum] = text.value;
-    saveTasks();
-  });
+function renderTask(taskNum) {
+    const taskRow = document.createElement("div")
 
-  const deleteBtn = document.createElement("input");
-  deleteBtn.type = "button";
-  deleteBtn.value = "X";
-  deleteBtn.addEventListener("click", () => {
-    deleteTask(taskNum);
-  });
+    const text = document.createElement("input")
+    text.type = "text"
+    text.placeholder = "Enter a task..."
+    text.value = tasks[taskNum]
+    text.className = "task-input"
+    text.addEventListener("change", () => {
+        tasks[taskNum] = text.value
+        saveTasks()
+    })
 
-  taskRow.appendChild(text);
-  taskRow.appendChild(deleteBtn);
+    const deleteBtn = document.createElement("input")
+    deleteBtn.type = "button"
+    deleteBtn.value = "X"
+    deleteBtn.className = "task-delete"
+    deleteBtn.addEventListener("click", () => {
+        deleteTask(taskNum)
+    })
 
-  const taskContainer = document.getElementById("task-container");
-  taskContainer.appendChild(taskRow);
+    taskRow.appendChild(text)
+    taskRow.appendChild(deleteBtn)
+
+    const taskContainer = document.getElementById("task-container")
+    taskContainer.appendChild(taskRow)
 }
 
 function addTask() {
-  const taskNum = tasks.length;
-  tasks.push("");
-  renderSingleTask(taskNum);
-  saveTasks();
+    const taskNum = tasks.length
+    tasks.push("")
+    renderTask(taskNum)
+    saveTasks()
 }
 
 function deleteTask(taskNum) {
-  tasks.splice(taskNum, 1);
-  renderTasks();
-  saveTasks();
+    tasks.splice(taskNum, 1)
+    renderTasks()
+    saveTasks()
 }
 
 function renderTasks() {
-  const taskContainer = document.getElementById("task-container");
-  taskContainer.textContent = "";
-  tasks.forEach((taskText, taskNum) => {
-    renderSingleTask(taskNum);
-  });
+    const taskContainer = document.getElementById("task-container")
+    taskContainer.textContent = ""
+    tasks.forEach((taskText, taskNum) => {
+        renderTask(taskNum)
+    })
 }
